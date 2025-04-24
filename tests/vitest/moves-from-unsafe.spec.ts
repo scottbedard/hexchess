d48bad302610b5c8c1a920db733059065aa669b5
Scott Bedard <scottbedard90@gmail.com>
2025-04-24 17:44:24 -0500

    tests

--- tests/vitest/moves-from-unsafe.spec.ts
+++ tests/vitest/moves-from-unsafe.spec.ts
@@ -12,10 +12,10 @@ describe('piece movement', () => {
     suite.forEach(spec => {
       describe(fixture.replaceAll('-', ' ').slice(0, -5), () => {
         test(spec.description, () => {
-          const hexchess = Hexchess.parse(spec.hexchess)
-          const results = hexchess.movesFromUnsafe(index(spec.from)).map(String)
+          const hexchess = Hexchess.parse(spec.from)
+          const results = hexchess.movesFromUnsafe(index(spec.position)).map(String)
 
-          expect(results, `${fixture} : ${spec.description}`).toEqual(spec.result)
+          expect(results, `${fixture} : ${spec.description}`).toEqual(spec.expect)
         })
       })
     })
--- tests/vitest/moves.spec.ts
+++ tests/vitest/moves.spec.ts
@@ -16,10 +16,10 @@ describe('piece movement', () => {
     suite.forEach(spec => {
       describe(fixture.replaceAll('-', ' ').slice(0, -5), () => {
         test(spec.description, () => {
-          const hexchess = Hexchess.parse(spec.hexchess)
-          const results = hexchess.movesFrom(index(spec.from)).map(String)
+          const hexchess = Hexchess.parse(spec.from)
+          const results = hexchess.movesFrom(index(spec.position)).map(String)
 
-          expect(results, `${fixture} : ${spec.description}`).toEqual(spec.result)
+          expect(results, `${fixture} : ${spec.description}`).toEqual(spec.expect)
         })
       })
     })
