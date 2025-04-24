0389859859b72692a8e2c36f91bc754636fc0245
Scott Bedard <scottbedard90@gmail.com>
2025-04-24 17:46:55 -0500

    cleanup

--- vitest/hexchess.spec.ts
+++ vitest/hexchess.spec.ts
@@ -15,28 +15,4 @@ describe('Hexchess', () => {
     expect(clone.board).not.toBe(hexchess.board)
     expect(clone).not.toBe(hexchess)
   })
-
-  describe('movesFrom', () => {
-    test('cannot step out of a pin', () => {
-      const hexchess = Hexchess.parse('1/3/5/7/4K4/5R5/5q5/11/11/11/11 w - 0 1')
-      const moves = hexchess.movesFrom('f6')
-      expect(moves.length).toBe(1)
-      expect(moves[0].toString()).toBe('f6f5')
-    })
-
-    test('cannot self check on opponent\'s turn', () => {
-      const hexchess = Hexchess.parse('1/3/5/7/4K4/5R5/5q5/11/11/11/11 b - 0 1')
-      const moves = hexchess.movesFrom('f6')
-
-      expect(moves.length).toBe(1)
-      expect(moves[0].toString()).toBe('f6f5')
-    })
-
-    test('king cannot step into check', () => {
-      const hexchess = Hexchess.parse('K/3/2q2/7/9/11/11/11/11/11/11 w - 0 1')
-      const moves = hexchess.movesFrom('f11')
-
-      expect(moves.length).toBe(0)
-    })
-  })
 })
