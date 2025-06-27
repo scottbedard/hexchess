643b3d797cc9f23d07902ada55102d944e8d047e
Scott Bedard <scottbedard90@gmail.com>
2025-06-27 13:08:56 -0500

    use a shared knight graph

--- crate/benches/initial_position.rs
+++ crate/benches/initial_position.rs
@@ -10,7 +10,7 @@ use test::Bencher;
 fn bench_initial_position(b: &mut Bencher) {
     // 18,182,566.70 ns/iter (+/- 311,939.48)
     // 17,940,987.40 ns/iter (+/- 331,128.34) - pre-calc pawns
-    // 840,047.90 ns/iter (+/- 24,078.53) - test unsafe from king's perspective
+    // 815,047.90 ns/iter (+/- 24,078.53) - test unsafe from king's perspective
     b.iter(|| {
         let hexchess_0 = Hexchess::init();
         let white_moves = hexchess_0.current_moves();
