cc6d35bc174914027e4a47242aaa377c002a9b78
Scott Bedard <scottbedard90@gmail.com>
2025-07-23 19:49:51 -0500

    move build to script

---
+++ scripts/build.ts
@@ -0,0 +1,37 @@
+import { copy, dim, execAsync, green, read, resolve, write } from './utils'
+import ora from 'ora'
+
+async function run() {
+  const startAt = Date.now()
+  const spinner = ora('Building...').start()
+
+  // cleanup
+  await execAsync(`rm -rf ${resolve('js/dist')}`, {
+    cwd: 'js',
+  })
+
+  // generate types
+  await execAsync('tsc --project ./tsconfig.json --emitDeclarationOnly', {
+    cwd: 'js',
+  })
+
+  // build npm package
+  await execAsync('npx rollup --config rollup.config.ts --configPlugin @rollup/plugin-typescript', {
+    cwd: 'js',
+  })
+
+  copy('LICENSE', 'js/dist/LICENSE')
+  copy('js/package.json', 'js/dist/package.json')
+  copy('js/README.md', 'js/dist/README.md')
+
+  // set version numbers
+  const pkg = JSON.parse(read('js/package.json'))
+  write('js/dist/index.mjs', read('js/dist/index.mjs').replace('x.y.z', pkg.version))
+
+  spinner.succeed('Done')
+
+  console.log()
+  console.log(`${green('Done')} ${dim(`${(Date.now() - startAt) / 1000}s`)}`)
+}
+
+run()
