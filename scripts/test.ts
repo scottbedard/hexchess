7ab45f38c7b9b464dd813f6141e46640bed5d1f0
Scott Bedard <scottbedard90@gmail.com>
2025-07-23 21:48:19 -0500

    tests

--- scripts/test.ts
+++ scripts/test.ts
@@ -8,6 +8,13 @@ async function testJs() {
   })
 }
 
+async function testPhp() {
+  await execAsync('./vendor/bin/pest --log-junit ./junit.xml --cache-directory=./.pest/cache', {
+    cwd: resolve('php'),
+    encoding: 'utf-8',
+  })
+}
+
 async function testRust() {
   await execAsync('cargo test --all-features', {
     cwd: resolve('rust'),
@@ -18,17 +25,23 @@ async function testRust() {
 async function run(args: string[]) {
   const startAt = Date.now()
 
-  if (args.includes('--js-only') || args.length === 0) {
+  if (args.includes('--js') || args.length === 0) {
     const js = ora('JavaScript...').start()
     await testJs()
     js.succeed(`JavaScript ${dim(`(${Date.now() - startAt}ms)`)}`)
   }
 
-  if (args.includes('--rs-only') || args.length === 0) {
+  if (args.includes('--rs') || args.length === 0) {
     const rust = ora('Rust...').start()
     const rustStart = Date.now()
     await testRust()
     rust.succeed(`Rust ${dim(`(${Date.now() - rustStart}ms)`)}`)
+  }
+
+  if (args.includes('--php') || args.length === 0) {
+    const php = ora('PHP...').start()
+    await testPhp()
+    php.succeed(`PHP ${dim(`(${Date.now() - startAt}ms)`)}`)
   }
 
   console.log()
