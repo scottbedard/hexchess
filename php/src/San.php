1186ef3ff38d9c3faf27c9f4f0ce04af4eb906c3
Scott Bedard <scottbedard90@gmail.com>
2025-07-24 10:23:46 -0500

    scaffold constructor

--- php/src/San.php
+++ php/src/San.php
@@ -10,11 +10,26 @@ class San
 
     public int $to;
 
+    /**
+     * Create a new san instance
+     *
+     * @param int|string $from
+     * @param int|string $to
+     * @param int|null $promotion
+     */
     public function __construct(
         int|string $from,
         int|string $to,
         $promotion = null
     ) {
-        // ...
+        $this->from = is_string($from)
+            ? array_search($from, Constants::POSITIONS, true)
+            : $from;
+
+        $this->to = is_string($to)
+            ? array_search($to, Constants::POSITIONS, true)
+            : $to;
+
+        $this->promotion = $promotion;
     }
 }
