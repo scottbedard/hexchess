64ad9c4013d236dca56f3dee2997221658b190de
Scott Bedard <scottbedard90@gmail.com>
2025-07-23 21:30:52 -0500

    scaffolding

--- php/src/Hexchess.php
+++ php/src/Hexchess.php
@@ -3,5 +3,11 @@
 namespace Bedard\Hexchess;
 
 class Hexchess {
-    // ...
+    public function add(int $a, int $b): int {
+        return $a + $b;
+    }
+
+    public function subtract(int $a, int $b): int {
+        return $a - $b;
+    }
 }
