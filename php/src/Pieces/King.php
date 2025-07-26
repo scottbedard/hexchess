2c35fc50639d27ac9f0fb7ddb6339f3a6505b10e
Scott Bedard <scottbedard90@gmail.com>
2025-07-25 21:39:35 -0500

    king moves

--- php/src/Pieces/King.php
+++ php/src/Pieces/King.php
@@ -2,12 +2,24 @@
 
 namespace Bedard\Hexchess\Pieces;
 
+use Bedard\Hexchess\Board;
 use Bedard\Hexchess\Hexchess;
+use Bedard\Hexchess\San;
 
 class King
 {
     public static function moves(Hexchess $hexchess, int $from, string $color): array
     {
-        return [];
+        $moves = [];
+
+        for ($i = 0; $i < 12; $i++) {
+            $to = Board::step($from, $i);
+
+            if ($hexchess->board[$to] === null || Board::color($hexchess->board[$to]) !== $color) {
+                $moves[] = new San($from, $to);
+            }
+        }
+
+        return $moves;
     }
 }
