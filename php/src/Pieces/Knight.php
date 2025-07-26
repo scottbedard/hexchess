2b4c2d30ffe019c87ccb87793cb129b5fccd3d2b
Scott Bedard <scottbedard90@gmail.com>
2025-07-25 21:32:06 -0500

    knight moves

--- php/src/Pieces/Knight.php
+++ php/src/Pieces/Knight.php
@@ -2,12 +2,25 @@
 
 namespace Bedard\Hexchess\Pieces;
 
+use Bedard\Hexchess\Board;
+use Bedard\Hexchess\Constants;
 use Bedard\Hexchess\Hexchess;
+use Bedard\Hexchess\San;
 
 class Knight
 {
     public static function moves(Hexchess $hexchess, int $from, string $color): array
     {
-        return [];
+        $moves = [];
+
+        $graph = Constants::KNIGHT_GRAPH[$from];
+
+        foreach ($graph as $to) {
+            if ($hexchess->board[$to] === null || Board::color($hexchess->board[$to]) !== $color) {
+                $moves[] = new San($from, $to);
+            }
+        }
+
+        return $moves;
     }
 }
