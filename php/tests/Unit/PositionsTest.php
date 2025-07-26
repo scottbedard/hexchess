810b4feafef266135daf3ce160189baa9d8a0889
Scott Bedard <scottbedard90@gmail.com>
2025-07-25 20:44:24 -0500

    positions

--- php/tests/Unit/PositionsTest.php
+++ php/tests/Unit/PositionsTest.php
@@ -1,5 +1,10 @@
 <?php
 
-use Bedard\Hexchess\Hexchess;
+use Bedard\Hexchess\Board;
+use Bedard\Hexchess\Constants;
 
-// ...
+foreach (Constants::POSITIONS as $index => $position) {
+    test("position {$position}", function () use ($index, $position) {
+        expect(Board::position($index))->toBe($position);
+    });
+}
