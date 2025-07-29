66b7b9e70a66639dd7199caa8a4188e4db9bb665
Scott Bedard <scottbedard90@gmail.com>
2025-07-28 19:52:08 -0500

    wip

--- rust/src/hexchess/bitboard.rs
+++ rust/src/hexchess/bitboard.rs
@@ -2,6 +2,26 @@ use crate::constants::{Color, Piece};
 use crate::hexchess::hexchess::stringify_board;
 use serde::{Deserialize, Serialize};
 
+/*
+https://www.redblobgames.com/grids/hexagons/#map-storage
+
+overview:
+                     f11                    
+                 e10 f10 g10                
+              d9  e9  f9  g9  h9            
+          c8  d8  e8  f8  g8  h8  i8        
+      b7  c7  d7  e7  f7  g7  h7  i7  k7    
+  a6  b6  c6  d6  e6  f6  g6  h6  i6  k6  l6
+  a5  b5  c5  d5  e5  f5  g5  h5  i5  k5  l5
+  a4  b4  c4  d4  e4  f4  g4  h4  i4  k4  l4
+  a3  b3  c3  d3  e3  f3  g3  h3  i3  k3  l3
+  a2  b2  c2  d2  e2  f2  g2  h2  i2  k2  l2
+  a1  b1  c1  d1  e1  f1  g1  h1  i1  k1  l1
+*/
+
+/// 11x11 bitboard mask, 0 = empty, 1 = off board
+const BOARD_MASK: u128 = 0b1111101111111110001111111000001111100000001110000000001000000000000000000000000000000000000000000000000000000000000000000;
+
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 struct Bitboard {
     pub black_bishop: u128, // turn color embedded at first byte, 0 = white, 1 = black
@@ -18,9 +38,7 @@ struct Bitboard {
     pub white_rook: u128,
 }
 
-
 impl Bitboard {
-
     /// all occupied positions
     pub fn all(&self) -> u128 {
         self.black_bishop |
@@ -53,6 +71,16 @@ impl Bitboard {
             0 => Color::Black,
             _ => Color::White,
         }
+    }
+
+    /// get the en passant index, embedded in black king
+    pub fn en_passant(&self) -> u8 {
+        (self.black_bishop & 0xFF) as u8
+    }
+
+    /// get the fullmove number, embedded in black pawn
+    pub fn fullmove(&self) -> u16 {
+        (self.black_pawn >> 112) as u16
     }
 
     /// get piece at a given index
@@ -96,6 +124,21 @@ impl Bitboard {
         None
     }
 
+    /// get the halfmove clock, embedded in black knight
+    pub fn halfmove(&self) -> u16 {
+        (self.black_knight >> 112) as u16
+    }
+
+    /// increment the fullmove clock, embedded in black pawn
+    pub fn inc_fullmove(&mut self) {
+        self.set_fullmove(self.fullmove().wrapping_add(1));
+    }
+
+    /// increment the halfmove clock, embedded in black knight
+    pub fn inc_halfmove(&mut self) {
+        self.set_halfmove(self.halfmove().wrapping_add(1));
+    }
+
     /// starting position
     pub fn init() -> Self {
         Self {
@@ -133,7 +176,7 @@ impl Bitboard {
     }
 
     /// set a piece at a given index
-    fn set(&mut self, piece: Piece, index: usize) {
+    pub fn set(&mut self, piece: Piece, index: usize) {
         match piece {
             Piece::BlackBishop => self.black_bishop |= 1 << index,
             Piece::BlackKing => self.black_king |= 1 << index,
@@ -150,7 +193,7 @@ impl Bitboard {
         };
     }
 
-    /// set the current turn color
+    /// set the current turn color, embedded in black bishop
     pub fn set_color(&mut self, color: Color) {
         let byte: u8 = match color {
             Color::Black => 0,
@@ -158,6 +201,23 @@ impl Bitboard {
         };
     
         self.black_bishop = (self.black_bishop & !0xFF) | (byte as u128);
+    }
+
+    /// set the en passant index, embedded in black king
+    pub fn set_en_passant(&mut self, index: u8) {
+        self.black_king = (self.black_king & !0xFF) | (index as u128);
+    }
+
+    /// set the halfmove clock, embedded in black knight
+    pub fn set_halfmove(&mut self, value: u16) {
+        let mask: u128 = 0xFFFF << 112;
+        self.black_knight = (self.black_knight & !mask) | ((value as u128) << 112);
+    }
+
+    /// set the fullmove clock, embedded in black pawn
+    pub fn set_fullmove(&mut self, value: u16) {
+        let mask: u128 = 0xFFFF << 112;
+        self.black_pawn = (self.black_pawn & !mask) | ((value as u128) << 112);
     }
 
     /// format as fen string
@@ -211,7 +271,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_set_arbitrary_values() {
+    fn test_arbitrary_positions() {
         let mut bitboard = Bitboard::new();
 
         bitboard.set(Piece::BlackBishop, 0);
@@ -281,6 +341,49 @@ mod tests {
 
         bitboard.set_color(Color::Black);
         assert_eq!(bitboard.color(), Color::Black);
+    }
+
+    #[test]
+    fn test_embedded_halfmove() {
+        let mut bitboard = Bitboard::init();
+        assert_eq!(bitboard.halfmove(), 0);
+
+        bitboard.set_halfmove(1);
+        assert_eq!(bitboard.halfmove(), 1);
+
+        bitboard.inc_halfmove();
+        assert_eq!(bitboard.halfmove(), 2);
+
+        bitboard.set_halfmove(1000);
+        assert_eq!(bitboard.halfmove(), 1000);
+
+        bitboard.set_halfmove(u16::MAX);
+        assert_eq!(bitboard.halfmove(), u16::MAX);
+    }
+
+    #[test]
+    fn test_embedded_fullmove() {
+        let mut bitboard = Bitboard::init();
+        assert_eq!(bitboard.fullmove(), 0);
+
+        bitboard.set_fullmove(1);
+        assert_eq!(bitboard.fullmove(), 1);
+
+        bitboard.inc_fullmove();
+        assert_eq!(bitboard.fullmove(), 2);
+
+        bitboard.set_fullmove(1000);
+        assert_eq!(bitboard.fullmove(), 1000);
+
+        bitboard.set_fullmove(u16::MAX);
+        assert_eq!(bitboard.fullmove(), u16::MAX);
+    }
+
+    #[test]
+    fn test_embedded_en_passant() {
+        let mut bitboard = Bitboard::init();
+        panic!("bitboard: {:?}", bitboard.en_passant());
+        assert_eq!(bitboard.en_passant(), 0);
     }
 
     #[test]
