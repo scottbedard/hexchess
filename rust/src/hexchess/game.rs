c62152d65435aca6508a315b8a77d92402cf2133
Scott Bedard <scottbedard90@gmail.com>
2025-08-06 10:34:49 -0500

    bishop threat

--- rust/src/hexchess/game.rs
+++ rust/src/hexchess/game.rs
@@ -334,9 +334,17 @@ impl Game {
             None => return false,
         };
 
+        let all_pieces = self.get_all_bitboard();
+
         let hostile_color = friendly_color.opposite();
 
         let (
+            friendly_bishops,
+            friendly_king,
+            friendly_knights,
+            friendly_pawns,
+            friendly_queens,
+            friendly_rooks,
             hostile_bishops,
             hostile_king,
             hostile_knights,
@@ -345,6 +353,12 @@ impl Game {
             hostile_rooks,
         ) = match friendly_color {
             Color::Black => (
+                *self.bitboard_black_bishop,
+                *self.bitboard_black_king,
+                *self.bitboard_black_knight,
+                *self.bitboard_black_pawn,
+                *self.bitboard_black_queen,
+                *self.bitboard_black_rook,
                 *self.bitboard_white_bishop,
                 *self.bitboard_white_king,
                 *self.bitboard_white_knight,
@@ -353,6 +367,12 @@ impl Game {
                 *self.bitboard_white_rook,
             ),
             Color::White => (
+                *self.bitboard_white_bishop,
+                *self.bitboard_white_king,
+                *self.bitboard_white_knight,
+                *self.bitboard_white_pawn,
+                *self.bitboard_white_queen,
+                *self.bitboard_white_rook,
                 *self.bitboard_black_bishop,
                 *self.bitboard_black_king,
                 *self.bitboard_black_knight,
@@ -378,20 +398,31 @@ impl Game {
         let diagonal_threats = hostile_bishops | hostile_queens;
 
         if diagonal_bitmask & diagonal_threats > 0 {
+            let (bishop, queen) = match hostile_color {
+                Color::Black => (Piece::BlackBishop, Piece::BlackBishop),
+                Color::White => (Piece::WhiteBishop, Piece::WhiteQueen),
+            };
+
             for n in [1u8, 3, 5, 7, 9, 11] {
                 let mut next_position = position.step(n);
 
                 loop {
                     match next_position {
-                        Some(p) => {
-                            if diagonal_threats & p.to_bitmask() > 0 {
+                        Some(next_p) => {
+                            let next_value = self.get_position(next_p);
+
+                            if next_value == Some(bishop) || next_value == Some(queen) {
                                 return true;
                             }
 
-                            next_position = p.step(n);
-                        }
+                            if next_value.is_some() {
+                                break;
+                            }
+
+                            next_position = next_p.step(n);
+                        },
                         None => break, // end of board
-                    }
+                    };
                 }
             }
         }
