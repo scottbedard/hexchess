33aad41776ee120e69c99ba3df3a7b291a48c613
Scott Bedard <scottbedard90@gmail.com>
2025-08-20 20:34:28 -0500

    versions

--- scripts/versions.js
+++ scripts/versions.js
@@ -6,14 +6,16 @@ export function versions(options) {
   const npm = JSON.parse(read('js/package.json'))
   const composer = JSON.parse(read('composer.json'))
   const cargoVersion = cargo.package.version
+  const macrosVersion = toml.parse(read('rust/macros/Cargo.toml')).package.version
 
   console.log('Checking versions...')
   console.log()
-  console.log(`NPM:      ${npm.version}`)
-  console.log(`Cargo:    ${cargoVersion}`)
-  console.log(`Composer: ${composer.version}`)
+  console.log(`NPM:             ${npm.version}`)
+  console.log(`Composer:        ${composer.version}`)
+  console.log(`Cargo:           ${cargoVersion}`)
+  console.log(`Cargo (macros):  ${macrosVersion}`)
 
-  const versions = [composer.version, npm.version, cargoVersion]
+  const versions = [composer.version, npm.version, cargoVersion, macrosVersion]
 
   if (!versions.every(val => val === versions[0])) {
     throw new Error(`Version mismatch [npm: ${npm.version}, cargo: ${cargoVersion}, composer: ${composer.version}]`)
