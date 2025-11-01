6ebd7d2a845314d50718093a0d89487c23c05f56
Scott Bedard <scottbedard90@gmail.com>
2025-10-31 22:59:08 -0500

    play button

--- docs/sandbox/Sandbox.vue
+++ docs/sandbox/Sandbox.vue
@@ -38,6 +38,14 @@
       
         Flip
       </button>
+
+      <button
+        class="flex gap-x-1.5 items-center text-sm tracking-wide hover:text-(--vp-code-color)!"
+        @click="onPlayClick">
+        <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 8V4H8"/><rect width="16" height="12" x="4" y="8" rx="2"/><path d="M2 14h2"/><path d="M20 14h2"/><path d="M15 13v2"/><path d="M9 13v2"/></svg>
+
+        Play
+      </button>
     </div>
   </div>
 </template>
@@ -116,6 +124,10 @@ function onClearClick() {
 
 function onFlipClick() {
   flipped.value = !flipped.value
+}
+
+function onPlayClick() {
+  console.log('play')
 }
 
 function onPositionClick(position: number) {
