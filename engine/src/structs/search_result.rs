6d9cade53eb75ba963fd7a280f7de0bc4b5d7a27
Scott Bedard <scottbedard90@gmail.com>
2025-11-08 10:38:51 -0600

    add engine test

--- engine/src/structs/search_result.rs
+++ engine/src/structs/search_result.rs
@@ -20,8 +20,26 @@ pub struct SearchResult {
 #[tsify(into_wasm_abi, from_wasm_abi)]
 pub struct ScoredSan {
     /// fen of the position
+    #[serde(serialize_with = "serialize_san_as_string")]
     pub san: San,
 
     /// score of the position
     pub score: f32,
+}
+
+fn serialize_san_as_string<S>(san: &San, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&san.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_san_from_and_to_string() {
+        assert_eq!(1, 2);
+    }
 }
