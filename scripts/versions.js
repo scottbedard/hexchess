86f1d233b8b9b9e20cb671d756684079df026007
Scott Bedard <scottbedard90@gmail.com>
2025-11-16 16:44:04 -0600

    add engine to version command

--- scripts/versions.js
+++ scripts/versions.js
@@ -3,8 +3,9 @@ import toml from 'smol-toml'
 
 export function versions(options) {
   const cargo = toml.parse(read('rust/Cargo.toml'))
-  const npm = JSON.parse(read('js/package.json'))
   const composer = JSON.parse(read('composer.json'))
+  const engine = JSON.parse(read('engine/package.json'))
+  const npm = JSON.parse(read('js/package.json'))
   const cargoVersion = cargo.package.version
   const macrosVersion = toml.parse(read('rust/macros/Cargo.toml')).package.version
 
@@ -14,11 +15,18 @@ export function versions(options) {
   console.log(`Composer:        ${composer.version}`)
   console.log(`Cargo:           ${cargoVersion}`)
   console.log(`Cargo (macros):  ${macrosVersion}`)
+  console.log(`Engine:          ${engine.version}`)
 
-  const versions = [composer.version, npm.version, cargoVersion, macrosVersion]
+  const versions = [
+    composer.version,
+    npm.version,
+    cargoVersion,
+    macrosVersion,
+    engine.version.split('-').shift(),
+  ]
 
   if (!versions.every(val => val === versions[0])) {
-    throw new Error(`Version mismatch [npm: ${npm.version}, cargo: ${cargoVersion}, composer: ${composer.version}]`)
+    throw new Error(`Version mismatch [npm: ${npm.version}, cargo: ${cargoVersion}, composer: ${composer.version}, engine: ${engine.version}]`)
   }
 
   if (options?.release) {
