a8cc1aa5df2cda4574185da5229a27cda3893a48
Scott Bedard <scottbedard90@gmail.com>
2025-11-18 19:43:32 -0600

    scaffold ui library

---
+++ ui/src/elements/hexboard.ts
@@ -0,0 +1,5 @@
+export class Hexboard extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
---
+++ ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  plugins: [
+    dts({
+      include: ['src/**/*'],
+      outDir: 'dist',
+      rollupTypes: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@bedard/hexchess-ui',
+      fileName: 'index',
+      formats: ['es']
+    },
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
+
